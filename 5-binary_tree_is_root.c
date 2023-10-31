a42195a6e1135243a3d0cdb830d6493dbac26aa8
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-10-31 06:45:11 -0700

    Is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,6 +10,10 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (0);
-	else
+	else if (node->parent != NULL)
+	{
 		return (1);
+	}
+
+	return (0);
 }
