57918e923b031aa1139b57d66f6087e1b00e68ad
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-10-31 09:37:26 -0700

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t leftheight;
+	size_t rightheight;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	size_t leftheight = binary_tree_height(tree->left);
-	size_t rightheight = binary_tree_height(tree->right);
+	leftheight = binary_tree_height(tree->left);
+	rightheight = binary_tree_height(tree->right);
 
 	if (leftheight > rightheight)
 	{
