6ffe80f33daacb19e0acb07438319e7b7cd407dd
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-11-01 03:08:20 -0700

    depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,20 +9,16 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		size_t depth = 0;
-		const binary_tree_t *current = tree;
+	size_t depth = 0;
+	const binary_tree_t *current = tree;
 
-		while (current->parent != NULL)
-		{
-			depth++;
-			current = current->parent;
-		}
-		return (current);
+	if (tree == NULL)
+		return (0);
+
+	while (current->parent != NULL)
+	{
+		depth++;
+		current = current->parent;
 	}
+	return (depth);
 }
