f58699fe70ff8c6e2b06589ba215056316d03c74
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-11-01 02:04:48 -0700

    height

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: a pointer to the node to measure the depth
+ * Return: depth of a node in a binary tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		size_t depth = 0;
+		const binary_tree_t *current = tree;
+
+		while (current->parent != NULL)
+		{
+			depth++;
+			current = current->parent;
+		}
+		return (current);
+	}
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,28 +4,20 @@
 
 /**
  * binary_tree_height - a function that measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height.
- * Return: height of the binary tree
+ * @tree: a pointer to the root node of the tree to measure the height
+ * Return: height of the tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t leftheight;
-	size_t rightheight;
-
 	if (tree == NULL)
 	{
 		return (0);
 	}
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
 
-	leftheight = binary_tree_height(tree->left);
-	rightheight = binary_tree_height(tree->right);
-
-	if (leftheight > rightheight)
-	{
-		return (leftheight + 1);
-	}
+	if (left_height > right_height)
+		return left_height + 1;
 	else
-	{
-		return (rightheight + 1);
-	}
+		return right_height + 1;
 }
